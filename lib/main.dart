f5a5f66903dd5fd154921c1f25be99a6b3767e98
faresarar <faresarar455@gmail.com>
2024-08-16 12:27:43 +0100

    change theme and create notes_screen.dart

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'screens/notes_screen.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const NotesScreen(),
+    );
+  }
+}
