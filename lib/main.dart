81010efcc7a56431df16878523f4f095f037662e
faresarar <faresarar455@gmail.com>
2024-08-16 12:42:14 +0100

    create AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class NotesApp extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       home: const NotesScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/notes_screen_body.dart';
+
 class NotesScreen extends StatelessWidget {
   const NotesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 7,
+        title: const Text(
+          "Notes",
+          style: TextStyle(
+            fontSize: 32,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          )
+        ],
+        centerTitle: true,
+      ),
+      body: const NotesScreenBody(),
+    );
   }
 }
