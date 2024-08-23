1cc65906f42fcec7e3cfbee95fe86202ef72e357
faresarar <faresarar455@gmail.com>
2024-08-23 19:04:46 +0100

    edit notes screen done

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -31,5 +31,5 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => throw UnimplementedError();
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
--- lib/widgets/edit_note_screen_body.dart
+++ lib/widgets/edit_note_screen_body.dart
@@ -1,11 +1,26 @@
-
 import 'package:flutter/material.dart';
+
+import 'custom_text_field.dart';
 
 class EditNoteScreenBody extends StatelessWidget {
   const EditNoteScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          CustomTextField(
+            hint: "Title",
+          ),
+          SizedBox(height: 16),
+          CustomTextField(
+            hint: "Content",
+            maxLines: 5,
+          ),
+        ],
+      ),
+    );
   }
 }
