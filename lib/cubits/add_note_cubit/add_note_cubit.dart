6cf9d275a885eedfc58377c1c441c28d7bfbcc3d
faresarar <faresarar455@gmail.com>
2024-08-24 12:09:49 +0100

    create add note Method

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,13 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+import '../../constants.dart';
 import '../../models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  addNote(NoteModel note){
-
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      Box<dynamic> notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteSuccess());
+      await notesBox.add(note);
+    }  catch (error) {
+      AddNoteFailure(error.toString());
+    }
   }
 }
