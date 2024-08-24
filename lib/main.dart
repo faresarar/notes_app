d6007a6de287b208ce9acfe25c661d31f61fda60
faresarar <faresarar455@gmail.com>
2024-08-24 17:38:33 +0100

    refactor multiBloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 import 'constants.dart';
-import 'cubits/add_note_cubit/add_note_cubit.dart';
 import 'models/note_model.dart';
 import 'screens/notes_screen.dart';
 import 'simple_bloc_observer.dart';
@@ -11,8 +10,8 @@ import 'simple_bloc_observer.dart';
 void main() async {
   await Hive.initFlutter();
   Bloc.observer = SimpleBlocObserver();
-  await Hive.openBox<NoteModel>(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
   runApp(const NotesApp());
 }
 
@@ -20,19 +19,14 @@ class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AddNoteCubit()),
-      ],
-      child: MaterialApp(
-        title: 'Notes App',
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          fontFamily: "Poppins",
-        ),
-        home: const NotesScreen(),
-        debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: "Poppins",
       ),
+      home: const NotesScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
